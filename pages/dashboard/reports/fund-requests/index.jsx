e6afdf7ab677ed63b3724894f68bde72fd15c714
sangam-cm <sangam@cloudmagician.in>
2023-07-14 16:50:30 +0530

    fund req report update

--- pages/dashboard/reports/fund-requests/index.jsx
+++ pages/dashboard/reports/fund-requests/index.jsx
@@ -174,18 +174,12 @@ const FundRequests = () => {
         .then((result) => {
           BackendAxios.get(
             pageLink ||
-            `/api/admin/fetch-fund/all?from=${Formik.values.from + (Formik.values.from && ("T" + "00:00"))
-            }&to=${Formik.values.to + (Formik.values.to && ("T" + "23:59"))
-            }&userId=${result.data.data.id}&search=${Formik.values.search
-            }&status=${Formik.values.status
-            }&pageSize=200`
+            `/api/admin/fetch-fund/all?from=${Formik.values.from + (Formik.values.from && ("T" + "00:00"))}&to=${Formik.values.to + (Formik.values.to && ("T" + "23:59"))
+            }&userId=${result.data.data.id}&search=${Formik.values.search}&status=${Formik.values.status}&pageSize=200`
           )
             .then((res) => {
               BackendAxios.get(
-                `/api/admin/print-report?from=${Formik.values.from + (Formik.values.from && ("T" + "00:00"))
-                }&to=${Formik.values.to + (Formik.values.to && ("T" + "23:59"))
-                }&userId=${result.data.data.id}&search=${Formik.values.search
-                }&status=${Formik.values.status}&pageSize=`
+                `/api/admin/print-report?from=${Formik.values.from + (Formik.values.from && ("T" + "00:00"))}&to=${Formik.values.to + (Formik.values.to && ("T" + "23:59"))}&userId=${result.data.data.id}&search=${Formik.values.search}&status=${Formik.values.status}&pageSize=`
               ).then((response) => {
                 setPrintableRow(response.data);
               });
@@ -231,11 +225,10 @@ const FundRequests = () => {
         });
       return;
     }
-    await BackendAxios.get(
+    BackendAxios.get(
       `/api/admin/print-report?from=${Formik.values.from + (Formik.values.from && ("T" + "00:00"))
       }&to=${Formik.values.to + (Formik.values.to && ("T" + "23:59"))}&userId=${Formik.values.userId
-      }&search=${Formik.values.search}&status=${Formik.values.status
-      }&pageSize=`
+      }&search=${Formik.values.search}&status=${Formik.values.status}&pageSize=`
     )
       .then(async (response) => {
         setPrintableRow(response.data);
