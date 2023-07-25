fc11befacfd82f75690a7255cb180b7461af57e3
Dezynation <dezynationindia@gmail.com>
2023-07-25 16:16:37 +0530

    ledger print

---
+++ lib/utils/pdfExport/PdfDocument.jsx
@@ -0,0 +1,92 @@
+import {
+  Document,
+  Page,
+  Text,
+  View,
+  StyleSheet,
+} from "@react-pdf/renderer";
+
+const styles = StyleSheet.create({
+  page: {
+    padding: 20,
+  },
+  container: {
+    flexDirection: "row",
+  },
+  column: {
+    flexDirection: "row",
+    backgroundColor: "#525FE1",
+    color: "#FFF",
+  },
+  cell: {
+    flex: 1,
+    padding: 5,
+  },
+});
+
+const PdfDocument = ({ rowData, columnDefs }) => (
+  <Document>
+    <Page size="A4" orientation="landscape" style={styles.page}>
+      <View style={styles.column}>
+        {columnDefs
+          .filter((column) => {
+            if (
+              column.field != "metadata" &&
+              column.field != "name" &&
+              column.field != "receipt"
+            ) {
+              return column;
+            }
+          })
+          .map((column, key) => {
+            return (
+              <View style={styles.cell} key={key}>
+                <Text>{column?.headerName}</Text>
+              </View>
+            );
+          })}
+      </View>
+      <View style={styles.container}>
+        {rowData.map((data, key) => (
+          <View style={styles.container} key={key}>
+            <View style={styles.cell}>
+              <Text>{key + 1}</Text>
+            </View>
+            <View style={styles.cell}>
+              <Text>{data.transaction_id}</Text>
+            </View>
+            <View style={styles.cell}>
+              <Text>{data.debit_amount}</Text>
+            </View>
+            <View style={styles.cell}>
+              <Text>{data.credit_amount}</Text>
+            </View>
+            <View style={styles.cell}>
+              <Text>{data.opening_balance}</Text>
+            </View>
+            <View style={styles.cell}>
+              <Text>{data.closing_balance}</Text>
+            </View>
+            <View style={styles.cell}>
+              <Text>{data.transaction_for}</Text>
+            </View>
+            <View style={styles.cell}>
+              <Text>{data.service_type}</Text>
+            </View>
+            <View style={styles.cell}>
+              <Text>{JSON.parse(data.metadata).status}</Text>
+            </View>
+            <View style={styles.cell}>
+              <Text>{data.created_at}</Text>
+            </View>
+            <View style={styles.cell}>
+              <Text>{data.updated_at}</Text>
+            </View>
+          </View>
+        ))}
+      </View>
+    </Page>
+  </Document>
+);
+
+export default PdfDocument;
--- pages/dashboard/retailer-panel/dashboard/reports/transactions/ledger.jsx
+++ pages/dashboard/retailer-panel/dashboard/reports/transactions/ledger.jsx
@@ -41,97 +41,8 @@ import { toBlob } from "html-to-image";
 import { useFormik } from "formik";
 import Cookies from "js-cookie";
 import { DownloadTableExcel } from "react-export-table-to-excel";
-import {
-  PDFDownloadLink,
-  Document,
-  Page,
-  Text,
-  View,
-  StyleSheet,
-} from "@react-pdf/renderer";
-
-const styles = StyleSheet.create({
-  page: {
-    padding: 20,
-  },
-  container: {
-    flexDirection: "row",
-  },
-  column: {
-    flexDirection: "row",
-    backgroundColor: "#525FE1",
-    color: "#FFF",
-  },
-  cell: {
-    flex: 1,
-    padding: 5,
-  },
-});
-
-const PdfDocument = ({ rowData, columnDefs }) => (
-  <Document>
-    <Page size="A4" orientation="landscape" style={styles.page}>
-      <View style={styles.column}>
-        {columnDefs
-          .filter((column) => {
-            if (
-              column.field != "metadata" &&
-              column.field != "name" &&
-              column.field != "receipt"
-            ) {
-              return column;
-            }
-          })
-          .map((column, key) => {
-            return (
-              <View style={styles.cell} key={key}>
-                <Text>{column?.headerName}</Text>
-              </View>
-            );
-          })}
-      </View>
-      <View style={styles.container}>
-        {rowData.map((data, key) => (
-          <View style={styles.container} key={key}>
-            <View style={styles.cell}>
-              <Text>{key + 1}</Text>
-            </View>
-            <View style={styles.cell}>
-              <Text>{data.transaction_id}</Text>
-            </View>
-            <View style={styles.cell}>
-              <Text>{data.debit_amount}</Text>
-            </View>
-            <View style={styles.cell}>
-              <Text>{data.credit_amount}</Text>
-            </View>
-            <View style={styles.cell}>
-              <Text>{data.opening_balance}</Text>
-            </View>
-            <View style={styles.cell}>
-              <Text>{data.closing_balance}</Text>
-            </View>
-            <View style={styles.cell}>
-              <Text>{data.transaction_for}</Text>
-            </View>
-            <View style={styles.cell}>
-              <Text>{data.service_type}</Text>
-            </View>
-            <View style={styles.cell}>
-              <Text>{JSON.parse(data.metadata).status}</Text>
-            </View>
-            <View style={styles.cell}>
-              <Text>{data.created_at}</Text>
-            </View>
-            <View style={styles.cell}>
-              <Text>{data.updated_at}</Text>
-            </View>
-          </View>
-        ))}
-      </View>
-    </Page>
-  </Document>
-);
+import { PDFDownloadLink } from "@react-pdf/renderer";
+import PdfDocument from "@/lib/utils/pdfExport/PdfDocument";
 
 const ExportPDF = () => {
   const doc = new jsPDF("landscape");
@@ -418,7 +329,7 @@ const Index = () => {
             Export PDF
           </Button> */}
           <PDFDownloadLink
-            document={<PdfDocument rowData={rowData} />}
+            document={<PdfDocument rowData={rowData} columnDefs={columnDefs} />}
             fileName={`Ledger(${Cookies.get("viewUserId")}).pdf`}
           >
             {({ blob, url, loading, error }) =>
